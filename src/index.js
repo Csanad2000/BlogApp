06fa54ea5ec7d62a3e084987916954bd9e96a3e0
Csanad <csanad.soletormos@gmail.com>
2023-10-08 17:44:07 +0200

    refactoring and styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

19af9a13ea731ee4d8c1ea46c68caad71218ddf7
Csanad <csanad.soletormos@gmail.com>
2023-10-22 17:36:30 +0200

    first display

---
+++ src/components/LinkList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import LinkItem from './LinkItem';
+import {useQuery, gql} from '@apollo/client';
+
+const FEED_QUERY = gql`
+query {
+    feed {
+        links {
+            id
+            url
+            description
+        }
+    }
+}
+`;
+
+const LinkList = () => {
+    const {data} = useQuery(FEED_QUERY);
+
+    return (
+        <div>
+            {data &&
+            <>
+                {data.feed.links.map((link) => (
+                    <LinkItem key = {link.id} link = {link}/>
+                ))}
+            </>
+            }
+        </div>
+    );
+};
+
+export default LinkList;
